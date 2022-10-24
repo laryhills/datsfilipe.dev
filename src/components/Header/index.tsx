d2b5ceeed4f72e14d02e153f93e59af34c10a981
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-24 17:54:26 -0300

    feat(header): add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import { ToggleThemeBtn } from '../ToggleThemeBtn'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+
+export function Header () {
+  return (
+    <header className='p-2 w-screen z-10 bg-zinc-100 dark:bg-zinc-800 bg-opacity-60 dark:bg-opacity-60 flex items-center justify-between text-gray-900 dark:text-gray-50 absolute'>
+      <div className='flex items-center'>
+        <h2 className='font-mplus font-bold text-xl'>datsfilipe</h2>
+        <a
+          href='https://github.com/datsfilipe'
+          className='text-xl ml-3'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaGithub />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/datsfilipe'
+          className='text-xl ml-3'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href='https://twitter.com/datsfilipe1'
+          className='text-xl ml-3'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaTwitter />
+        </a>
+      </div>
+      <ToggleThemeBtn />
+    </header>
+  )
+}

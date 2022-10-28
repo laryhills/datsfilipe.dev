6861865571ed6711f732cf1859fdbca6ed7c8bc2
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-27 22:33:14 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Header } from './components/Header'
-import { MainComponent } from './components/MainContainer'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Posts from './pages/Posts'
+import Contact from './pages/Contact'
+import NoMatch from './pages/NoMatch'
 
 function App() {
   return (
-    <div className='w-full h-full bg-zinc-100 dark:bg-zinc-800 scroll-smooth flex flex-col'>
-      <Header />
-      <MainComponent />
+    <div className='w-full min-h-screen bg-zinc-100 dark:bg-zinc-800 scroll-smooth flex flex-col dark:text-neutral-50 text-gray-900'>
+       <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   )
 }

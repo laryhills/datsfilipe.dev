72c978e8706b8b44e7a74336c7ca5276657fc506
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-27 22:35:40 -0300

    fix(button): fix children type and add styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,12 @@
-import type { HTMLAttributes, ReactElement } from 'react'
+import type { HTMLAttributes, ReactNode } from 'react'
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
-  children: ReactElement;
+  children: ReactNode;
 }
 
 export function Button ({ children, ...props }: ButtonProps) {
   return (
-    <button {...props}>
+    <button {...props} className='bg-stone-700 text-neutral-50 hover:bg-stone-800 dark:bg-zinc-300 dark:hover:bg-neutral-50 dark:text-stone-700 text-xl font-mplus font-medium rounded-xl transition-all px-5 py-4 flex items-center'>
       {children}
     </button> 
   )

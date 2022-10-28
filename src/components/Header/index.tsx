c0ccd194edad25f0fb7fe61cf33846825ad52660
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-27 22:36:32 -0300

    fix: fix styles for header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 export function Header () {
   return (
-    <header className='p-2 w-screen z-10 bg-zinc-100 dark:bg-zinc-800 bg-opacity-60 dark:bg-opacity-60 flex items-center justify-between text-gray-900 dark:text-gray-50 absolute'>
+    <header className='p-2 w-screen z-10 bg-zinc-100 dark:bg-zinc-800 bg-opacity-60 dark:bg-opacity-60 flex items-center justify-between text-gray-900 dark:text-gray-50 fixed'>
       <div className='flex items-center'>
         <h2 className='font-mplus font-bold text-xl'>datsfilipe</h2>
         <a
           href='https://github.com/datsfilipe'
-          className='text-xl ml-3'
+          className='text-xl ml-3 hover:text-2xl transition-all dark:hover:text-neutral-300 hover:text-gray-500'
           target='_blank'
           rel='noreferrer'
         >
@@ -16,7 +16,7 @@ export function Header () {
         </a>
         <a
           href='https://www.linkedin.com/in/datsfilipe'
-          className='text-xl ml-3'
+          className='text-xl ml-3 hover:text-2xl transition-all dark:hover:text-neutral-300 hover:text-gray-500'
           target='_blank'
           rel='noreferrer'
         >
@@ -24,7 +24,7 @@ export function Header () {
         </a>
         <a
           href='https://twitter.com/datsfilipe1'
-          className='text-xl ml-3'
+          className='text-xl ml-3 hover:text-2xl transition-all dark:hover:text-neutral-300 hover:text-gray-500'
           target='_blank'
           rel='noreferrer'
         >

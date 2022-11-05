3a788f992e270f29a9f368d0dd2233d988ee19eb
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-05 14:24:19 -0300

    feat(toggle theme button): add animation

--- src/components/ToggleThemeBtn/index.tsx
+++ src/components/ToggleThemeBtn/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { motion } from 'framer-motion'
 import { useState, useEffect } from 'react'
 import { HiSun, HiMoon } from 'react-icons/hi'
 
@@ -10,6 +11,15 @@ export function ToggleThemeBtn () {
     if (window.matchMedia('(prefers-colorscheme: dark)').matches) return 'dark'
     return 'light'
   })
+
+  const variants = {
+    light: {
+      rotate: 0
+    },
+    dark: {
+      rotate: 360
+    }
+  }
 
   const toggleTheme = () => {
     if (theme === 'light') {
@@ -32,20 +42,27 @@ export function ToggleThemeBtn () {
 
   return (
     <div className='flex justify-between items-center p-1 rounded-xl w-fit bg-zinc-100'>
-      <button
+      <motion.button
         onClick={toggleTheme}
         aria-label='Toggle Theme'
         className='flex justify-center items-center p-2 mr-1 bg-amber-100 rounded-lg text-neutral-900 dark:bg-zinc-100'
+        variants={variants}
+        initial='light'
+        animate={theme}
+        transition={{ duration: 0.3 }}
       >
         <HiSun />
-      </button>
-      <button
+      </motion.button>
+      <motion.button
         onClick={toggleTheme}
         aria-label='Toggle Theme'
         className='flex justify-center items-center p-2 rounded-lg bg-zinc-100 text-neutral-900 dark:bg-zinc-300'
+        variants={variants}
+        initial='light'
+        animate={theme}
       >
         <HiMoon />
-      </button>
+      </motion.button>
     </div>
   )
 }

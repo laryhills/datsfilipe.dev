73b1791bbe0709e73f3647bcd9838dc4a86e03c7
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-05 14:24:51 -0300

    feat(navigation): hide and show navigation component based on scroll behavior

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,10 +1,57 @@
+'use client'
+
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
 import { Button } from '../Button'
 import { FaHome, FaUser, FaCommentAlt, FaEnvelope } from 'react-icons/fa'
 
 export function Navigation () {
+  // the animation should trigger on scroll, if down, then hide the nav, if up, then show the nav
+  const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up')
+  const [lastScrollPosition, setLastScrollPosition] = useState(0)
+
+  const handleScroll = () => {
+    const currentScrollPosition = window.pageYOffset
+    if (currentScrollPosition < 0) {
+      setScrollDirection('up')
+    } else if (currentScrollPosition > lastScrollPosition) {
+      setScrollDirection('down')
+    } else {
+      setScrollDirection('up')
+    }
+    setLastScrollPosition(currentScrollPosition)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [lastScrollPosition, scrollDirection])
+
+  const variants = {
+    up: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.3
+      }
+    },
+    down: {
+      y: '-100%',
+      opacity: 0,
+      transition: {
+        duration: 0.3
+      }
+    }
+  }
+
   return (
-    <div className='flex fixed bottom-4 z-10 justify-center items-center w-screen'>
+    <motion.div
+      className='flex fixed bottom-4 z-10 justify-center items-center w-screen'
+      variants={variants}
+      initial='up'
+      animate={scrollDirection}
+    >
       <nav>
         <ul className='flex'>
           <li className='mx-5'>
@@ -41,6 +88,6 @@ export function Navigation () {
           </li>
         </ul>
       </nav>
-    </div>
+    </motion.div>
   )
 }

d3b0f440e3a7f2d743053cafd0daa16aced6f383
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 20:06:49 -0300

    feat(next fonts): use next fonts to add fonts to app and add next font config to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Inter, M_PLUS_Rounded_1c, Merriweather } from '@next/font/google'
+
 import { CopyrightMark } from '../components/CopyrightMark'
 import { Header } from '../components/Header'
 
@@ -8,22 +10,33 @@ interface RootLayoutProps {
   children: ReactElement;
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--inter-font'
+})
+
+const mplus = M_PLUS_Rounded_1c({
+  subsets: ['latin'],
+  weight: ['500', '700'],
+  variable: '--mplus-font'
+})
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  variable: '--merriweather-font',
+  weight: ['400', '700']
+})
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>datsfilipe website</title>
-
-        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body>
-        <div className='flex flex-col w-full min-h-screen selection:bg-orange-500 selection:bg-opacity-30 h-fit scroll-smooth bg-neutral-50 text-neutral-900 dark:text-neutral-50 dark:bg-neutral-900'>
+        <div className={`${inter.variable} ${mplus.variable} ${merriweather.variable} flex flex-col w-full min-h-screen selection:bg-orange-500 selection:bg-opacity-30 h-fit scroll-smooth bg-neutral-50 text-neutral-900 dark:text-neutral-50 dark:bg-neutral-900`}>
           <Header />
           {children}
           <CopyrightMark />

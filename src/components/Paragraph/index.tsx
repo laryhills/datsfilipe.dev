cc5e0233fe470912d2d4e052a71fbbd66a69b89b
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 02:34:07 -0300

    feat(paragraph): add paragraph component

---
+++ src/components/Paragraph/index.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes, ReactNode } from 'react'
+
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement> {
+  children: ReactNode;
+}
+
+export function Paragraph ({ children, ...props }: ParagraphProps) {
+  const { className, ...rest } = props
+
+  return (
+    <p className={`text-justify ${className}`} {...rest}>{children}</p>
+  )
+}

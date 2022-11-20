78ec5738b9556d0e86792ba23d7f9e6d2f389a35
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 19:27:49 -0300

    feat(contact page): add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,27 @@
+import { Title } from '../../components/Title'
+
+export default function Contact () {
+  return (
+    <div className='flex flex-col justify-center items-center mx-10 min-h-screen'>
+      <main className='flex flex-col items-center w-full max-w-3xl'>
+        <Title className='my-12 text-6xl'>Say Hello!</Title>
+        <form className='flex flex-col [&>*]:mb-4 w-full'>
+          <label htmlFor='email' className='font-medium'>Your email:</label>
+          <input
+            type='email'
+            id='email'
+            className='p-4 rounded-3xl border-2 border-stone-400 text-neutral-900 placeholder:text-neutral-400 placeholder:italic bg-neutral-100 dark:text-neutral-50 dark:placeholder:text-neutral-500 dark:border-stone-700 dark:bg-neutral-800'
+            placeholder='datsfilipe.dev@gmail.com'
+          />
+          <label htmlFor='message' className='font-medium'>Your message</label>
+          <textarea
+            id='message'
+            className='p-4 rounded-3xl border-2 resize-none text-neutral-900 placeholder:text-neutral-400 placeholder:italic border-stone-400 bg-neutral-100 dark:text-neutral-50 dark:placeholder:text-neutral-500 dark:border-stone-700 dark:bg-neutral-800'
+            rows={6}
+            placeholder='Hi, how are you? ...'
+          />
+        </form>
+      </main>
+    </div>
+  )
+}

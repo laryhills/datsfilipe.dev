a91adcf8d5690937a42937e233c89af1bae7b8d6
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 02:39:22 -0300

    refactor(copyright mark): change the margin since navigation component is up in the header now

--- src/components/CopyrightMark/index.tsx
+++ src/components/CopyrightMark/index.tsx
@@ -5,7 +5,7 @@ export function CopyrightMark() {
   const pathname = usePathname()
 
   return (
-    <p className={`my-4 mx-auto ${pathname === '/' ? 'absolute bottom-16 self-center text-stone-500': ''}`}>
+    <p className={`my-4 mx-auto ${pathname === '/' ? 'absolute bottom-4 self-center text-stone-500': ''}`}>
       © 2021 datsfilipe. All rights reserved.
     </p>
   )

56d3a6ec16e06f6b986bd07abba8b2097436bed2
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 16:25:18 -0300

    fix(custom link): add stop propagation to avoid open cards by clicking links and set 'use client'

--- src/components/CustomLink/index.tsx
+++ src/components/CustomLink/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { BsArrowUpRight } from 'react-icons/bs'
 import { HTMLAttributes, ReactNode } from 'react'
 
@@ -9,7 +10,7 @@ interface CustomLinkProps extends HTMLAttributes<HTMLAnchorElement> {
 
 export function CustomLink ({ children, ...props }: CustomLinkProps) {
   return (
-    <a {...props} className='inline-flex items-center transition-all hover:underline w-fit group text-neutral-800 dark:text-neutral-50'>
+    <a onClick={(e) => e.stopPropagation()} {...props} className='inline-flex items-center transition-all hover:underline w-fit group text-neutral-800 dark:text-neutral-50'>
       {children}
       &nbsp;
       <BsArrowUpRight className='transition-all group-hover:ml-1' />

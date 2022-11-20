9c589c302a749b636f2e111a3c9526aff2433808
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 02:33:47 -0300

    feat(section): add section component

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion'
+
+import { HTMLAttributes, ReactNode } from 'react'
+
+interface SectionProps extends HTMLAttributes<HTMLTableSectionElement> {
+  children?: ReactNode;
+}
+
+export function Section ({ children, ...props }: SectionProps) {
+  const { className, ...rest } = props
+
+  return (
+    <section className={`flex items-center min-h-screen ${className}`} {...rest}>
+      {children}
+    </section>
+  )
+}

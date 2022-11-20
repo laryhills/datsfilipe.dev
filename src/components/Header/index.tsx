e03b5546a9e1457275e9a9d516296a60f512fd13
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-20 20:37:15 -0300

    feat(header component): add responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,10 @@ export function Header () {
   return (
     <header className='flex fixed z-10 justify-between items-center p-4 w-screen bg-opacity-20 dark:bg-opacity-5 bg-neutral-100 backdrop-blur-sm dark:bg-neutral-700'>
       <div className='flex items-center'>
-        <h2 className='text-xl font-bold text-orange-500 font-mplus'>datsfilipe</h2>
+        <h2 className='text-xl font-bold text-orange-500 max-lg:hidden font-mplus'>datsfilipe</h2>
         <a
           href='https://github.com/datsfilipe'
-          className='ml-3 text-xl transition-all hover:text-2xl hover:text-orange-500 text-stone-600 dark:text-neutral-300 hover:dark:text-orange-500'
+          className='ml-3 text-xl transition-all hover:text-2xl hover:text-orange-500 max-lg:ml-0 text-stone-600 dark:text-neutral-300 hover:dark:text-orange-500'
           target='_blank'
           rel='noreferrer'
         >

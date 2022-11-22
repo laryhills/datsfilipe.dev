9dc101cc49a5893d7c2bfb6d75aa31cca40bcdc7
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-21 22:31:04 -0300

    feat(button component): update styles and add animations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,24 @@
-import type { HTMLAttributes, ReactNode } from 'react'
+'use client'
+import { motion } from 'framer-motion'
+import type { ReactNode, HTMLAttributes } from 'react'
+import type { MotionProps } from 'framer-motion'
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export function Button ({ children, ...props }: ButtonProps) {
+export function Button ({ children, ...props }: ButtonProps & MotionProps) {
+  const { className, ...rest } = props
+
   return (
-    <button {...props} className='flex items-center py-2 px-4 text-base font-medium rounded-md transition-all text-stone-300 bg-neutral-700 dark:bg-stone-400 dark:text-neutral-900 hover:brightness-125 hover:translate-y-[-4px]'>
+    <motion.button
+      {...rest}
+      className={`flex items-center py-2 px-8 text-lg font-medium rounded-xl hover:cursor-pointer w-fit bg-stone-300 border border-stone-400 dark:border-stone-500 text-neutral-900 dark:text-stone-300 dark:bg-neutral-700 ${className ? className : ''}`}
+      whileTap={{ scale: 0.95 }}
+      whileHover={{ scale: 1.05 }}
+    >
       {children}
-    </button> 
+    </motion.button> 
   )
 }

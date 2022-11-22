efc0baebc4eea99064f91d9a02d0bc64f67f378a
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-22 00:29:11 -0300

    fix(bad apple component): fix build error
    it seems to be a problem with bad apple component export, gonna look at it some time, that way it
    works

--- src/components/BadApple/index.tsx
+++ src/components/BadApple/index.tsx
@@ -1,6 +1,6 @@
 'use client'
-
-import { BadApple as BadAppleComponent } from '@datsfilipe/react-bad-apple'
+// it seems that are some problems with my component export declarations ? gonna look at it some time
+import { BadApple as BadAppleComponent } from '@datsfilipe/react-bad-apple/dist/esm/index'
 
 export function BadApple () {
   return (

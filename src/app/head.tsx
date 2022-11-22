2b5be5bbc35bbea89412cd41cd5f10fed47236c2
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-22 18:19:50 -0300

    feat(next/head): add head for pages

---
+++ src/app/head.tsx
@@ -0,0 +1,22 @@
+export default function Head () {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <title>datsfilipe</title>
+      <link rel="icon" href="/icon.png" type="image/png" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Filipe's personal website" />
+      <meta name="author" content="datsfilipe" />
+      <meta name="keywords" content="filipe, datsfilipe, personal, website" />
+
+      <meta property="og:title" content="datsfilipe" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://datsfilipe.dev" />
+      <meta property="og:image" content="https://datsfilipe.dev/og-image.png" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://datsfilipe.dev" />
+      <meta property="twitter:image" content="https://datsfilipe.dev/og-image.png" />
+    </>
+  )
+}

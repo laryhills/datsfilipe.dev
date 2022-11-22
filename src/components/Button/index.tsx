8c29e3473697e7d15cfaa28d55dba3d6ba648258
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-22 12:34:29 -0300

    refactor(button component): change styles and remove animations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,24 +1,19 @@
-'use client'
-import { motion } from 'framer-motion'
 import type { ReactNode, HTMLAttributes } from 'react'
-import type { MotionProps } from 'framer-motion'
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   type?: 'button' | 'submit' | 'reset';
 }
 
-export function Button ({ children, ...props }: ButtonProps & MotionProps) {
+export function Button ({ children, ...props }: ButtonProps) {
   const { className, ...rest } = props
 
   return (
-    <motion.button
+    <button
       {...rest}
-      className={`flex items-center py-2 px-8 text-lg font-medium rounded-xl hover:cursor-pointer w-fit bg-stone-300 border border-stone-400 dark:border-stone-500 text-neutral-900 dark:text-stone-300 dark:bg-neutral-700 ${className ? className : ''}`}
-      whileTap={{ scale: 0.95 }}
-      whileHover={{ scale: 1.05 }}
+      className={`flex items-center py-2 px-8 text-lg font-medium rounded-xl transition-all hover:brightness-90 hover:cursor-pointer w-fit bg-stone-300 border border-stone-400 dark:border-stone-500 text-neutral-900 dark:text-stone-300 dark:bg-neutral-700 ${className ? className : ''}`}
     >
       {children}
-    </motion.button> 
+    </button> 
   )
 }

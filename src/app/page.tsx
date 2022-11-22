5bcf2f8b8019aa5061fe8f90affab2525183975e
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-21 22:35:23 -0300

    style(home page): just break lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Page() {
     <div className='flex flex-col justify-center items-center w-screen h-screen'>
       <div className='z-10 mx-4 text-7xl font-bold shadow-inner max-lg:text-5xl max-md:text-2xl max-md:mt-28 max-lg:mt-5 font-mplus'>
         <UserInfo />
-        <h1 className='mt-6 text-orange-500 dark:text-orange-600'>Filipe Lima,</h1>
+        <h1 className='mt-6 text-orange-500 dark:text-orange-600'>
+          Filipe Lima,
+        </h1>
         <h1>Web Developer,</h1>
         <h1>I&apos;m making what I love &lt;3</h1>
         <div className='flex text-neutral-600 dark:text-neutral-500'>
